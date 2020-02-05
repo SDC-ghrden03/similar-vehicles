bd1f6f6da2f12f072f18e627afc956fd72fb1634
lizanaya92 <lizanaya92@gmail.com>
2020-02-05 09:23:04 -0700

    restructured react architecture to split up components. OnMouse over, each component changes state

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import SimilarVehicles from './SimilarVehicles.jsx';
 
-
-class App extends React.Component{
+class App extends React.Component {
     constructor(props) {
         super(props); 
         this.state = {
             similarVehicles: [], 
-            hover: false
+            hoverOne: false, 
+            hoverTwo: false, 
+            hoverThree: false,
+            isLoading: false
         }; 
 
-        this.onMouseOut = this.onMouseOut.bind(this); 
-        this.onMouseOver = this.onMouseOver.bind(this); 
+        this.onMouseOutOne = this.onMouseOutOne.bind(this); 
+        this.onMouseOverOne = this.onMouseOverOne.bind(this); 
+        this.onMouseOutTwo = this.onMouseOutTwo.bind(this); 
+        this.onMouseOverTwo = this.onMouseOverTwo.bind(this); 
+        this.onMouseOutThree = this.onMouseOutThree.bind(this); 
+        this.onMouseOverThree = this.onMouseOverThree.bind(this); 
         this.getSimilarVehicles = this.getSimilarVehicles.bind(this); 
     } 
 
     componentDidMount() {
-        this.getSimilarVehicles()
+        this.getSimilarVehicles();
     } 
 
-    getSimilarVehicles () {
+    getSimilarVehicles() {
+        this.setState({isLoading: true});
         fetch('http://localhost:3000/api/similar_vehicles')
         .then((results) => {
            return results.json()
         })
         .then((data) => {
             this.setState({
-                similarVehicles: data.results
-            })
+                similarVehicles: data.results,
+                isLoading: false
+            });
         })
         .catch((err) => {
             if (err) {
@@ -37,33 +45,79 @@ class App extends React.Component{
 
     }; 
 
-    onMouseOver() {
+    onMouseOverOne() {
         
         this.setState(
             {
-                hover: true
+                hoverOne: true
             }
         )
     }; 
 
-    onMouseOut() {
+    onMouseOutOne() {
         
         this.setState(
             {
-                hover: false
+                hoverOne: false
             }
         )
     }; 
+
+    onMouseOverTwo() {
+        
+        this.setState(
+            {
+                hoverTwo: true
+            }
+        )
+    }; 
+
+    onMouseOutTwo() {
+        
+        this.setState(
+            {
+                hoverTwo: false
+            }
+        )
+    };
+    
+    onMouseOverThree() {
+        
+        this.setState(
+            {
+                hoverThree: true
+            }
+        )
+    }; 
+
+    onMouseOutThree() {
+        
+        this.setState(
+            {
+                hoverThree: false
+            }
+        )
+    };
 
     render() {
+        const { similarVehicles, hoverOne, hoverTwo, hoverThree, isLoading} = this.state;
         return(
+            !isLoading ? 
             <div className="similarVehiclesContainer">
                 <SimilarVehicles 
-                similarVehicles={this.state.similarVehicles} 
-                hover={this.state.hover} 
-                onMouseOver={this.onMouseOver} 
-                onMouseOut={this.onMouseOut} /> 
-            </div>
+                    similarVehicles={similarVehicles} 
+                    hoverOne={hoverOne} 
+                    hoverTwo={hoverTwo}
+                    hoverThree={hoverThree}  
+                    onMouseOverOne={this.onMouseOverOne} 
+                    onMouseOutOne={this.onMouseOutOne}
+                    onMouseOverTwo={this.onMouseOverTwo} 
+                    onMouseOutTwo={this.onMouseOutTwo}
+                    onMouseOverThree={this.onMouseOverThree} 
+                    onMouseOutThree={this.onMouseOutThree} 
+                /> 
+            </div> : 
+            <div><h1>Loading...</h1></div>
         ); 
     } 
 } 
---
+++ client/src/components/ThreeSimilarVehicle.jsx
@@ -0,0 +1,69 @@
+import React from 'react'; 
+
+const ThreeSimilarVehicle = (props) => {
+    
+    if(props.hover === false) {
+        return (
+        <div className="car hvr-sweep-to-top" onMouseOver={props.onMouseOver}>
+            <div className="media">
+                <img
+                    src={props.picture}
+                    alt=""
+                    width="300" 
+                    height="230"
+                />
+            </div>
+            <div className="info">
+                <h3 className="first-glance-description">
+                    <a className="car-description">
+                    {props.year} {props.make} {props.model} {props.class}
+                    <br />
+                    <br />
+                    </a>
+                    <a>{props.price}</a>
+                </h3>
+            </div>
+        </div>
+        );
+    } else {
+        return(
+            <div className="desc car" onMouseOut={props.onMouseOut}>
+                <h3 className="hoverDescription mouseOverInfo">
+                    <a className="car-description">
+                        {props.year} {props.make} {props.model} {props.class}
+                    </a>
+                    <a>{props.price}</a>
+                </h3>
+                <ul className="similar-vehicles-details unordered-list">
+                <li className="odometer">
+                    <i className="fas fa-tachometer-alt fa-lg"></i>
+                    <span className="value">&nbsp; &nbsp; {props.miles} miles</span>
+                </li>
+                <li className="engine">
+                    <i className="fas fa-cogs fa-lg"></i>
+                    <span className="value">&nbsp; &nbsp; {props.engine_L_Cyl}</span>
+                </li>
+                <li className="transmission">
+                    <i className="fas fa-car-battery fa-lg"></i>
+                    <span className="value">&nbsp; &nbsp; {props.transmission}</span>
+                </li>
+                <li className="exteriorColor">
+                    <i className="fas fa-paint-roller fa-lg"></i>
+                    <span className="value">&nbsp; &nbsp; {props.exterior_color}</span>
+                </li>
+                <li className="interiorColor">
+                    <i className="fas fa-paint-brush fa-lg"></i>
+                    <span className="value">&nbsp; &nbsp; {props.interior_color}</span>
+                </li>
+                </ul>
+
+                <a className="view-details" href="https://www.echopark.com/">
+                    View Details
+                </a>
+            </div>
+        )
+    }
+             
+}; 
+
+export default ThreeSimilarVehicle; 
