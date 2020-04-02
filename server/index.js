e03f0e9eba7b57ec1002a04665c76678e2390ece
jim walter <jimwalter13090@gmail.com>
2020-04-02 11:22:30 -0600

    PUT method added

--- server/index.js
+++ server/index.js
@@ -27,17 +27,14 @@ connection.connect((err) => {
 }); 
 
 app.get('/api/similar_vehicles', (req, res) => {
-
+    // randomly generate a car type
     var types = ['Sedan', 'SUV', 'Coupe', 'Hatchback', 'Truck', 'Van', 'Wagon', 'Other'];
-
     var index = Math.floor(Math.random() * types.length);
-
     var carType = types[index];
-
-
-
+    console.log(carType);
+    
+    // query the DB
     const getQueryString = `SELECT * FROM vehicle WHERE class = "${carType}" LIMIT 3`; 
-
     connection.query(getQueryString, (err, results) => {
         if (err) {
             res.status(400).send(err);
@@ -47,7 +44,8 @@ app.get('/api/similar_vehicles', (req, res) => {
     }); 
 }); 
 
-app.post('/api/add_vehicle', (req, res) => {
+app.post('/api/similar_vehicles/add_vehicle', (req, res) => {
+
     const bod = req.query;
     const postQueryString = `INSERT INTO vehicle (year, make, model, class, price, miles, engine_L_Cyl, transmission, exterior_color, interior_color, picture) VALUES (${bod.year}, ${bod.make}, ${bod.model}, ${bod.class}, ${bod.price}, ${bod.miles}, ${bod.engine_L_Cyl}, ${bod.transmission}, ${bod.exterior_color}, ${bod.interior_color}, ${bod.picture})`
     
@@ -60,9 +58,24 @@ app.post('/api/add_vehicle', (req, res) => {
     });
 });
 
-app.put('/api/mod_vehicle', (req, res) => {    });
+app.put('/api/similar_vehicles/mod_vehicle', (req, res) => {
+    
+    console.log(req.query);
 
-app.delete('/api/delete_vehicle', (req, res) => {    }); 
+    const putQueryString = `UPDATE vehicle SET make = 'Chevy' WHERE id = 104`
+
+    connection.query(putQueryString, (err, results) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.status(200).send('row updated');
+        }
+    });
+});
+
+app.delete('/api/similar_vehicles/delete_vehicle', (req, res) => {
+    
+}); 
 
 app.listen(PORT, () => {
     console.log(`Listening from: ${PORT}`); 
