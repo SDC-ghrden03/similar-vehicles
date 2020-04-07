9e5ed592b0e193c7bd06f2148029c0ca5f21f456
jim walter <jimwalter13090@gmail.com>
2020-04-07 17:40:17 -0600

    mongo seed created. Achieves 10M records into DB in 3min 30sec

--- db/index.js
+++ db/index.js
@@ -2,11 +2,19 @@ const mongoose = require('mongoose');
 
 const mongoURI = 'mongodb://localhost:27017/similarVehicles';
 
-mongoose.connect(mongoURI, { useNewUrlParser: true});
+const db = mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true});
 
-var gooseDB = mongoose.connection;
-gooseDB.on('error', console.error.bind(console, 'Connection error:'));
-gooseDB.once('Connection Open', () => console.log('Connected to mongoDB'));
+db
+  .then(db => console.log(`Connected to: ${mongoURI}`))
+  .catch(err => {
+    console.log(`There was a problem connecting to mongo at: ${mongoURI}`);
+    console.log(err);
+  });
+
+// var gooseDB = mongoose.connection;
+
+// gooseDB.on('error', console.error.bind(console, 'Connection error:'));
+// gooseDB.once('Connection Open', () => console.log('Connected to mongoDB'));
 
 const similarVehicleSchema = new mongoose.Schema({
     year: Number,
@@ -24,3 +32,16 @@ const similarVehicleSchema = new mongoose.Schema({
 
 const Vehicle = mongoose.model('Vehicle', similarVehicleSchema);
 
+const addManyVehicles = (cars) => {
+    return new Promise((resolve, reject) => {
+        Vehicle.collection.insertMany(cars, (err, docs) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(docs.length, 'added');
+            }
+        })
+    })
+};
+
+module.exports = addManyVehicles;
---
+++ db/mongoSeed.js
@@ -0,0 +1,50 @@
+const faker = require('faker');
+const pics = require('../seedPictures.js');
+const addManyVehicles = require('./index.js');
+
+// Vehicle constants for generating random vehicles
+const makes = ['Acura', 'Audi', 'BMW', 'Buick', 'Cadillac', 'Chevrolet', 'Chrysler', 'Dodge', 'Ford', 'GMC', 'Honda', 'Hyundai', 'Jeep', 'Kia', 'Lexus', 'Lincoln', 'Mazda', 'Mercedes', 'Nissan', 'Porsche', 'Ram', 'Subaru', 'Tesla', 'Toyota', 'Volkswagen', 'Volvo'];
+const classes = ['Sedan', 'SUV', 'Coupe', 'Hatchback', 'Truck', 'Van', 'Wagon', 'Other'];
+const engines = ['3.5L V-6 Cyl', '2.4L I-4 Cyl','1.8L I-4 Cyl', '2.0L H-4 Cyl', '3.6L V-6 Cyl', '2.7L V-6 Cyl', '4.3L V-6 Cyl'];
+const transmissions = ['Automatic', 'Manual'];
+
+// fake vehicle object generating function
+const oneFakeVehicle = () => {
+  result = {};
+  result.year = Math.floor(Math.random() * (2020 - 1991)) + 1991;
+  result.make = makes[Math.floor(Math.random() * makes.length)];
+  result.model = faker.name.firstName();
+  result.class = classes[Math.floor(Math.random() * classes.length)];
+  result.price = '$' + faker.commerce.price();
+  result.miles = faker.random.number();
+  result.engine_L_Cyl = engines[Math.floor(Math.random() * engines.length)];
+  result.transmission = transmissions[Math.floor(Math.random() * transmissions.length)];
+  result.exterior_color = faker.commerce.color();
+  result.interior_color = faker.commerce.color();
+  result.picture = pics.pics[Math.floor(Math.random() * pics.pics.length)];
+
+  return result;
+}
+
+function batch() {
+    var batchOfVehicles = [];
+    // # of vehicles to generate
+    var vehicleCount = 10000;
+    for (var i = 0; i < vehicleCount; i++) {
+        batchOfVehicles.push(oneFakeVehicle());
+    }
+    return batchOfVehicles;
+}
+
+async function seedVehicles() {
+    var loopCount = 0;
+    while (loopCount < 1000) {
+        var thisBatch = batch();
+        await addManyVehicles(thisBatch);
+        loopCount++;
+    }
+}
+
+seedVehicles()
+    .then(data => console.log('SEED COMPLETE'))
+    .catch(err => console.log(err));
